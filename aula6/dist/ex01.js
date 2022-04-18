f71b42cf77d946e1cba85cb8e0fb53f351511403
Diego Mendes Carloni <83482281+diegocarloni@users.noreply.github.com>
2022-04-18 15:59:25 -0300

    novos exemplos

---
+++ aula6/dist/ex01.js
@@ -0,0 +1,78 @@
+"use strict";
+/*
+class Pessoa {
+            //invariante: nome.length > 0 && idade > 0
+            private nome: string;
+            private idade: number;
+
+        constructor(nome: string, idade: number) {
+            if (nome.length == 0)
+                throw new Error('Nome inválido');
+            if (idade < 0)
+                throw new Error('Nome inválido');
+            this.nome = nome;
+            this.idade = idade;
+        }
+
+        getNome() { return this.nome; }
+}
+
+const joao = new Pessoa('Joao', 20);
+*/
+/*
+class Pessoa {
+    //invariante: nome.length > 0 && idade > 0
+    private _nome: string;
+    private _idade: number;
+
+constructor(nome: string, idade: number) {
+    if (nome.length == 0)
+        throw new Error('Nome inválido');
+    if (idade < 0)
+        throw new Error('Nome inválido');
+    this._nome = nome;
+    this._idade = idade;
+}
+
+    get nome() { return this._nome; }
+    set nome(umNome: string) {
+        if (umNome.length == 0)
+            throw new Error ('Nome invalido');
+            this._nome = umNome;
+        }
+    get idade() { return this._idade; }
+}
+
+const joao = new Pessoa('Joao', 20);
+joao.nome = ''; // joao.nome('Joao Manuel')
+console.log(joao.nome);
+*/
+/*
+class Pessoa {
+        constructor(public nome: string, public idade: number) {}
+}
+
+const joao = new Pessoa('Joao', 10);
+console.log(joao.nome)
+*/
+class Pessoa {
+    //invariante: nome.length > 0 && idade > 0
+    constructor(_nome, _idade) {
+        this._nome = _nome;
+        this._idade = _idade;
+        if (this._nome.length == 0)
+            throw new Error('Nome inválido');
+        if (this._idade < 0)
+            throw new Error('Nome inválido');
+    }
+    get nome() { return this._nome; }
+    set nome(umNome) {
+        if (umNome.length == 0)
+            throw new Error('Nome invalido');
+        this._nome = umNome;
+    }
+    get idade() { return this._idade; }
+}
+const maria = new Pessoa('Maria', 20);
+console.log(maria.nome);
+maria.nome = 'Maria Jose';
---
+++ aula6/dist/ex02.js
@@ -0,0 +1,56 @@
+"use strict";
+/*
+const set = new Set<number>();
+set.add(10);
+set.add(20);
+set.add(10);
+console.log(set);
+*/
+/*
+class NSet {
+        // O array elements nao contem elementos repetidos
+        private _elements: number[] = [];
+        get elements() { return this._elements; }
+}
+
+const s = new NSet ();
+const es = s.elements;
+es.push(10);
+es.push(10);
+console.log(s.elements);
+
+//ERRADO !!!
+*/
+class NSet {
+    constructor(xs) {
+        // O array elements nao contem elementos repetidos
+        this.elements = [];
+        for (const x of xs)
+            this.add(x);
+    }
+    // Para cada objeto obj: NSet
+    // e cada x: number (do tipo number)  vale que:
+    // obj.add(x)
+    //obj.add(x) -> true
+    add(x) {
+        const idx = this.elements.indexOf(x);
+        if (idx == -1)
+            this.elements.push(x); // Se idx == -1 entao x nao e elemento de elements
+    }
+    has(x) {
+        return this.elements.indexOf(x) != -1;
+    }
+    remove(x) {
+        const idx = this.elements.indexOf(x);
+        if (idx != -1) {
+            const last = this.elements.length - 1;
+            this.elements[idx] = this.elements[last];
+            this.elements.pop();
+        }
+    }
+}
+const conj = new NSet([1, 4, 2, 1, 1, 3]);
+conj.add(8);
+console.log(conj.has(8));
+conj.remove(1);
+console.log(conj.has(1));
